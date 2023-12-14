7c90c02c89dc65051fb5bb60d3b7e858fdc7c835
lee <lee@172.20.nate.com>
2023-12-15 02:03:24 +0900

    Initial commit

---
+++ GuardDuty-ThreatList-Update.py
@@ -0,0 +1,23 @@
+import boto3
+
+ACCESSKEY='AKIAUFU22QD7DC36PJNE'
+SECRETKEY='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'
+detectorid='8cc1253ac29f57732265130d3ac4e445'
+
+def GuardDuty_ThreatList_Update():
+    client = boto3.client('guardduty', region_name="ap-northeast-2", aws_access_key_id=ACCESSKEY, aws_secret_access_key=SECRETKEY)
+    TI_LIST = client.list_threat_intel_sets(DetectorId=detectorid, MaxResults=10)
+
+    print(TI_LIST)
+
+    ThreatID = TI_LIST['ThreatIntelSetIds'][0]
+    print(ThreatID)
+    TI_GET = client.get_threat_intel_set(DetectorId=detectorid, ThreatIntelSetId=ThreatID)
+    ThreatName = TI_GET['Name']
+    ThreatLocation = TI_GET['Location']
+    print(ThreatLocation)
+    TI_UPDATE = client.update_threat_intel_set(DetectorId=detectorid, ThreatIntelSetId=ThreatID, Name=ThreatName, Location=ThreatLocation, Activate=True)
+
+
+if __name__ == '__main__':
+    GuardDuty_ThreatList_Update()
